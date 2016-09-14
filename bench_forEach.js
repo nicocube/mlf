1d1d198756620abdb317509a873113a00f6a2636
Nicolas Lochet <nlo@connecthings.com>
2016-09-14 18:31:52 +0200

    add bench tests

---
+++ bench_forEach.js
@@ -0,0 +1,23 @@
+'use strict'
+ 
+var mlf = require(__dirname+'/lib/mlf.js')
+
+var a = [1, 42, 3, 5, 9, 70]
+  , b = mlf(a)
+
+exports.compare = {
+  "vanilla forEach" : function () {
+    var c = 0
+    a.forEach(function() { c+=1 })
+  },
+  "mlf forEach" : function () {
+    var c = 0
+    mlf(a).forEach(function() { c+=1 })
+  },
+  "mlf cached forEach" : function () {
+    var c = 0
+    b.forEach(function() { c+=1 })
+  }
+}
+
+require("bench").runMain()
---
+++ bench_reduce.js
@@ -0,0 +1,20 @@
+'use strict'
+ 
+var mlf = require(__dirname+'/lib/mlf.js')
+
+var a = [1, 42, 3, 5, 9, 70]
+  , b = mlf(a)
+
+exports.compare = {
+  "vanilla reduce" : function () {
+    a.reduce(function(a,b) {return a+b})
+  },
+  "mlf reduce" : function () {
+    mlf(a).reduce(function(a,b) {return a+b})
+  },
+  "mlf cached reduce" : function () {
+    b.reduce(function(a,b) {return a+b})
+  }
+}
+
+require("bench").runMain()
